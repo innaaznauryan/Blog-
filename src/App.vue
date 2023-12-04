bd4513eec51cdd518cb4a73e108c086cf1b1a1b0
innaaznauryan <inna.aznauryan@gmail.com>
2023-12-04 14:41:30 +0400

    no rights reserved

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <Nav/>
-  <router-view/>
+  <div :style="{ minHeight: 'calc(100vh - 130px)'}">
+    <router-view/>
+  </div>
+  <footer class="text-center p-10 text-stone-900">
+    <p class="font-medium">© Inna Aznauryan 2023</p>
+    <p>No rights reserved</p>
+  </footer>
 </template>
 
 <script setup>
