96d757f978e0a4de73d3d8aa4bda676526197c5d
innaaznauryan <inna.aznauryan@gmail.com>
2024-04-09 01:07:10 +0400

    text-overflow

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -2,13 +2,11 @@
   <router-link
       :to="{ name: 'single-post', params: {id: post.id} }"
       class="w-full sm:w-1/2 xl:w-1/3 2xl:w-1/4 p-2">
-    <div class="h-full bg-white shadow-lg">
-      <div class="p-4 duration-500 hover:text-[#44c744] flex flex-col gap-2">
-        <h2 class="font-medium text-lg">{{ post.title }}</h2>
-        <h3 class="font-medium text-md">{{ post.user }}</h3>
-        <p class="italic text-sm">{{ post.date }}</p>
-        <p>{{ post.content.slice(0, 50) }}...</p>
-      </div>
+    <div class="h-full p-4 flex flex-col gap-2 bg-white shadow-lg hover:text-[#44c744] duration-500">
+      <h2 class="font-medium text-lg">{{ post.title }}</h2>
+      <h3 class="font-medium text-md">{{ post.user }}</h3>
+      <p class="italic text-sm">{{ post.date }}</p>
+      <p class="text-ellipsis overflow-hidden whitespace-nowrap">{{ post.content }}</p>
     </div>
   </router-link>
 </template>
